0c7fea181908913c05ef55373451919efe013276
juan.durangom <juan.durangom@arus.com.co>
2022-08-04 11:31:06 -0500

    Septimo intento

---
+++ curso-VSCode-1.0.0/src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,26 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+function Hola() {
+    return 'Saludos';
+}
+
+
+
+
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
